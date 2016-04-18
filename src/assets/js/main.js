a683d2cb2c66c05341ca8fc67b5141f018348b5b
Drew Rawitz <email@drewrawitz.com>
2016-04-17 20:18:20 -0400

    adding some error styles

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,6 +14,18 @@
       return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
+    /**
+     * Validate a URL input
+     *
+     * @param {string} text - the url to validate
+     * @return {boolean} true or false
+     */
+    function validateURL(text) {
+      var urlregex = new RegExp(
+            "^(http:\/\/www.|https:\/\/www.|ftp:\/\/www.|www.){1}([0-9A-Za-z]+\.)");
+      return urlregex.test(text);
+    }
+
     $('a[data-breakpoint]').on('click', function(e) {
       e.preventDefault();
 
@@ -22,12 +34,34 @@
       if(breakpoint === 'random') {
         // let's create a random number. the max is the width of the document.
         breakpoint = getRandomInt(320, $(document).width());
-        console.log(breakpoint);
       }
 
       $('#screen-size').animate({
         width: breakpoint+'px'
       });
+    });
+
+    $('#site-input').on('submit', function(e) {
+      e.preventDefault();
+
+      var value = $('input[name=url]').val();
+
+      // if input is a valid URL
+      if(validateURL(value)) {
+
+        // remove any errors
+        $('.site-input__wrapper').removeClass('form-error');
+
+        // let's update the iFrame with the new src. prepend http:// if missing.
+        if (!/^http:\/\//.test(value)) {
+          value = "http://" + value;
+        }
+
+        $('.iframe__wrapper iframe').attr('src', value);
+      } else {
+        $('.site-input__wrapper').addClass('form-error');
+      }
+
     });
 
   });
